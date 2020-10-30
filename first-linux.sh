c3060638dab0fb10e31cd2b64fc9be872b6e14d9
kaitoryouga <kaito1477800@gmail.com>
2020-10-31 00:39:16 +0700

    fix token 2

--- first-linux.sh
+++ first-linux.sh
@@ -1,2 +1,2 @@
-echo "eba91818e378db27411a372c32909514351b889c" | docker login https://docker.pkg.github.com -u kaitoryouga --password-stdin
+echo "51508a44711456318f006dccef647268fea382c6" | docker login https://docker.pkg.github.com -u kaitoryouga --password-stdin
 docker-compose -f docker-compose.linux.yml up -d --build
