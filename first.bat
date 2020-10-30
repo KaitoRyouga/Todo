159e43a94ba086f27737c34158d38cc8ecdc263e
kaitoryouga <kaito1477800@gmail.com>
2020-10-31 00:49:21 +0700

    remove token

--- first.bat
+++ first.bat
@@ -1,2 +1,2 @@
-echo "a9903a66be7f3201819be233486738a019f4ee1a" | docker login https://docker.pkg.github.com -u kaitoryouga --password-stdin
+echo "%1" | docker login https://docker.pkg.github.com -u kaitoryouga --password-stdin
 docker-compose up -d --build
